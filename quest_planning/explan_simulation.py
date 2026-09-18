b58181d11b8b52a0f12d46d0bb8ee0fb78a7831b
QuESt Developer <quest_planning@sandia.gov>
2026-09-18 14:23:54 -0600

    updating explan_simulation

--- quest_planning/explan_simulation.py
+++ quest_planning/explan_simulation.py
@@ -87,7 +87,7 @@ class Explan:
         d.set_reserves_option(config['reserves_option'])
         d.set_tax_credits_option(config['tax_credits_option'])
         d.set_es_lifetime_cost_option(config['es_lifetime_cost_option'])
-        d.set_large_load_option(config['large_load_option'])
+        d.set_large_load_option(config.get('large_load_option', False))
         d.set_solver(config['solver'])
         d.set_system_name(config['system'])
         d.set_mva_base(config['mva_base'])
