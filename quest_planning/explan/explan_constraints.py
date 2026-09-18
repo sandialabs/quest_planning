7aae614921e992c8e69df740695390fbc32aff29
Cody Newlun <cjnewlu@sandia.gov>
2026-09-17 19:23:56 -0600

    add slack bus constraint

--- quest_planning/explan/explan_constraints.py
+++ quest_planning/explan/explan_constraints.py
@@ -905,7 +905,7 @@ class ExplanConstraints:
         tb = model.to_bus[l]
         theta_diff = model.theta[fb, y, s, i] - model.theta[tb, y, s, i]
         # Normalize by mva_base for numerical stability
-        return (1/self.mva_base) * model.PF[l, y, s, i] - (1/model.line_X[l]) * theta_diff == 0
+        return model.PF[l, y, s, i] - (self.mva_base/model.line_X[l]) * theta_diff == 0
         
     def cThetaDiffMax(self,model, l, y, s, i):
         theta_diff = model.theta[model.from_bus[l], y, s, i] - model.theta[model.to_bus[l], y, s, i]
